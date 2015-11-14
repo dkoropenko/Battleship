2edb96da92fb29ba9ee7d524893badbd7c35efbd
Smalew <nikotin13@mail.ru>
2015-11-14 16:04:23 +0300

    Переписал
    Весь код с 0

--- Game/src/Logic/Ship.java
+++ Game/src/Logic/Ship.java
@@ -1,27 +1,78 @@
 package Logic;
 
+import java.util.ArrayList;
+
 /**
  * Created by Диман on 12.11.2015.
  */
 public class Ship {
-    private int[] location;
+    private int type = 0;
+    private int hits = 0;
+    private int deck = 0;
+    private ArrayList<String[]> deckArrLoc;
+    String[] location;
 
-    public void setLocation(int[] x){
-        location = x;
+    public Ship(){
+        type = 0;
+        hits = 0;
+        deck = 0;
+        deckArrLoc = new ArrayList<>();
+    }
 
-        System.out.print("numbers: ");
-        for (int i = 0; i < this.location.length ; i++) {
-            System.out.print(this.location[i] + " ");
+    public void setOptions (int type, int deck){
+        this.type = type;
+        this.deck = deck;
+    }
+
+    //public  getLocation(){
+     //   return location;
+    //}
+
+    public void setLocation(int deck){
+        location= new String[2];
+
+        for (int i = 0; i < deck; i++) {
+            location[0] = "B";
+            location[1] = "1";
+
+            deckArrLoc.add(i,location);
         }
     }
 
-    public boolean checkHit(int location){
-        for (int i = 0; i < this.location.length ; i++) {
-            if (this.location[i] == location){
-                this.location[i] = 11;
-                return true;
+    public int getHits(){
+        return this.hits;
+    }
+
+    public int getDeck(){
+        return this.deck;
+    }
+
+    public int checkHits(String[] hit){
+
+        String[] checkHit = new String[];
+
+
+        for (int i = 0; i < this.deck; i++) {
+            checkHit = this.deckArrLoc.get(i);
+
+            if (checkHit[0].equals(hit[0]) && checkHit[1].equals(hit[1])){
+                return 1;
+            }
+            else {
+                return 0;
             }
         }
-        return false;
+
     }
+
+    /*
+    public int checkLive(){
+        if (this.hits == this.deck){
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }*/
+
 }
--- Game/src/Logic/Start.java
+++ Game/src/Logic/Start.java
@@ -6,37 +6,6 @@ package Logic;
 public class Start {
 
     public static void main(String[] args) {
-        Ship one = new Ship();
-
-        int[] location = {2,3,4};
-
-        one.setLocation(location);
-
-        int playerHit, hits=0, loose=0;
-
-        int x =0;
-        while(x != 7){
-
-            playerHit = x;
-
-            System.out.println("\nHit " + playerHit + ".");
-
-            if (one.checkHit(playerHit)) {
-                hits += 1;
-            }
-            else {
-                System.out.println("Try again!");
-                loose += 1;
-            }
-
-            if (hits == 3) {
-                System.out.println("Player wins.");
-                System.out.println("Missed: " + loose);
-                break;
-            }
-            x++;
-        }
-
-
+        Ship first = new Ship();
     }
 }
